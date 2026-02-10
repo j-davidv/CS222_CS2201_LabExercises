e602255a40f678ffed1797c39b3a5c4bcfc43424
JDV <98734554+j-davidv@users.noreply.github.com>
2026-02-10 09:11:34 +0800

    submission

---
+++ CS222_CS2201_LabExercises/VillanJohnDavidM/CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter amount in USD: ");
+double amount = double.Parse(Console.ReadLine());
+
+Console.Write("Enter exchange rate from USD to EUR: ");
+double rate = double.Parse(Console.ReadLine());
+
+double exchangeAmount = amount * rate;
+
+Console.Write("Amount in EUR: {0:F2}", exchangeAmount);
+
---
+++ CS222_CS2201_LabExercises/VillanJohnDavidM/InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter principal amount: ");
+double principal = double.Parse(Console.ReadLine());
+
+Console.Write("Enter rate of interest: ");
+double rate = double.Parse(Console.ReadLine());
+
+Console.Write("Enter time in years: ");
+double time = double.Parse(Console.ReadLine());
+
+double result = (principal * rate * time) / 100;
+
+Console.WriteLine($"Simple Interest: {result}");
