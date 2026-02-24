3b874103ba967b9e1f3d3853219a0ba5b23437d5
JDV <98734554+j-davidv@users.noreply.github.com>
2026-02-24 09:22:07 +0800

    submission

---
+++ CS222_CS2201_LabExercises/VillanJohnDavidM/Laboratory Activity 1 - 24-FEB-2026/01 - PentagonClass/PentagonClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Pentagon
+{
+    private double side;
+
+    public Pentagon()
+    {
+        this.side = 0;
+    }
+
+    public Pentagon(double side)
+    {
+        this.side = side;
+    }
+
+    public double GetPerimeter()
+    {
+        return 5 * this.side;
+    }
+
+    public double GetArea()
+    {
+        return (Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * side * side) / 4.0;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Side Length: {this.side}");
+        Console.WriteLine($"Perimeter: {GetPerimeter():F3}");
+        Console.WriteLine($"Area: {GetArea():F3}");
+    }
+}
+
+public class PentagonClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Pentagon p1");
+        Console.WriteLine("-------------------------");
+        Pentagon p1 = new Pentagon();
+        p1.Display();
+        Console.WriteLine();
+
+        Console.WriteLine("Pentagon p2");
+        Console.WriteLine("-------------------------");
+        Pentagon p2 = new Pentagon(5);
+        p2.Display();
+        Console.WriteLine();
+    }
+}
+
+
+        
---
+++ CS222_CS2201_LabExercises/VillanJohnDavidM/Laboratory Activity 1 - 24-FEB-2026/02 - Music Player Organizer/MusicPlayerOrganizer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+public class Song
+{
+    public string title;
+    public string artist;
+    public double duration;
+
+    public Song(string title, string artist, double duration)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
+    public Song(string title, string artist) : this(title, artist, 0.0)
+    {
+
+    }
+
+    public Song() : this("Unknown", "Unknown")
+    {
+
+    }
+
+    public void DisplaySong()
+    {
+        Console.WriteLine("{0,-20} {1,-20} {2,-20:F2}", title, artist, duration);
+    }
+}
+
+public class MusicPlayerOrganizer
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Songs to add: ");
+
+        int addSongs = int.Parse(Console.ReadLine());
+
+        Song[] songs = new Song[addSongs];
+
+        for (int i = 0; i < songs.Length; i++)
+        {
+            Console.WriteLine($"\nSong #{i + 1}");
+
+            try
+            {
+                Console.Write("Title: ");
+                string title = Console.ReadLine();
+
+                Console.Write("Artist: ");
+                string artist = Console.ReadLine();
+
+                Console.Write("Duration (minutes): ");
+
+                double duration = double.Parse(Console.ReadLine());
+
+                songs[i] = new Song(title, artist, duration);
+            }
+            catch (FormatException)
+            {
+                songs[i] = new Song();
+            }
+        }
+
+        Console.WriteLine("\n=== || MY PLAYLIST || ===");
+        Console.WriteLine("{0,-20} {1,-20} {2,-20}", "Title", "Artist", "Time");
+        Console.WriteLine("-------------------------------------------------");
+
+        double totalDuration = 0;
+
+        foreach (Song song in songs)
+        {
+            song.DisplaySong();
+            totalDuration += song.duration;
+        }
+
+        double avgDuration = 0;
+        if (songs.Length > 0)
+        {
+            avgDuration = totalDuration / songs.Length;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Total Duration: {totalDuration:F2} mins");
+        Console.WriteLine($"Average Duration: {avgDuration:F2} mins");
+    }
+}
